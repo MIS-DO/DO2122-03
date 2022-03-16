26520b39516800dd213d57b0337622dd75f4fbaf
José Enrique <87719556+josenggn@users.noreply.github.com>
2022-03-16 10:37:06 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #development
 export NODE_ENV=development
+export mongoDBHostname=mongo
 export PORT=8080
 export DBPORT=27018
 docker-compose -p acmeexplorer-devel up -d
@@ -7,6 +8,7 @@ docker-compose -p acmeexplorer-devel up -d
 
 #production
 export NODE_ENV=production
+export mongoDBHostname=mongo
 export PORT=80
 export DBPORT=27015
 docker-compose -p acmeexplorer-prod up -d
